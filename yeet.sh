4c5bc457eb5ade9844b18f8bbdd5034bc04fb52f
m5cott <mcscottbsn@outlook.com>
2020-11-28 18:45:14 -0600

    moved zsh config higher in script. Removed commented out installs.

--- yeet.sh
+++ yeet.sh
@@ -54,6 +54,13 @@ $priv snap connect qemu-virgil:removable-media
 mkdir -vp $HOME/.local/bin
 ./plebrice.sh
 
+# make zshenv read from new .zshrc location
+echo 'export ZDOTDIR="${XDG_CONFIG_HOME:-$HOME/.config}/zsh"' | sudo tee -a /etc/zsh/zshenv
+
+# zsh shell config
+mkdir -vp $HOME/.cache/zsh
+chsh -s $(which zsh)
+
 # Quickemu
 mkdir -vp $HOME/.local/src
 cd $HOME/.local/src && git clone https://github.com/wimpysworld/quickemu.git
@@ -69,41 +76,6 @@ curl https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.config/lf/l
 curl https://raw.githubusercontent.com/LukeSmithxyz/voidrice/master/.local/bin/lf-select -o $HOME/.local/bin/lf-select
 chmod +x $HOME/.local/bin/lf-select $HOME/.local/bin/rotdir
 
-# make zshenv read from new .zshrc location
-echo 'export ZDOTDIR="${XDG_CONFIG_HOME:-$HOME/.config}/zsh"' >/dev/null | sudo tee -a /etc/zsh/zshenv
-
-# zsh shell config
-mkdir -vp $HOME/.cache/zsh
-chsh -s $(which zsh)
-
 # youtube-dl
 #$priv curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 #$priv chmod a+rx /usr/local/bin/youtube-dl
-
-# Cascadia Code PL (Work in Progress)
-#wget https://github.com/microsoft/cascadia-code/releases/download/v2009.22/CascadiaCode-2009.22.zip
-#unzip CascadiaCode-2009.22.zip -d CascadiaCode
-
-# VS Code
-#wget -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868
-#$priv dpkg -i code.deb
-#rm -f code.deb
-
-# go lang
-#curl -L -o /tmp/go1.15.3.linux-amd64.tar.gz https://golang.org/dl/go1.15.3.linux-amd64.tar.gz
-#$priv tar -C /usr/local -xzf /tmp/go1.15.3.linux-amd64.tar.gz
-#$priv echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
-#source $HOME/.profile
-#go version
-#rm -rf /tmp/go1.15.3.linux-amd64.tar.gz
-
-# powerline-go
-#go get -u github.com/justjanne/powerline-go
-
-# pwsh
-#curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-x64.tar.gz
-#$priv mkdir -p /opt/microsoft/powershell/7
-#$priv tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
-#$priv chmod +x /opt/microsoft/powershell/7/pwsh
-#$priv ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
-#rm -rf /tmp/powershell.tar.gz
