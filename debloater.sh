e516622be8900e552eae56d9316ae3361a7dc47a
m5cott <mcscottbsn@outlook.com>
2020-12-16 19:18:52 -0600

    debian gnome debloated initial commit

---
+++ debloater.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+$distro=`lsb_release -is`
+
+if [ $distro = "Debian" ]; then
+    echo "Removing some bloat..."
+    echo "let's start with some games."
+    sleep 1.5
+
+    sudo apt remove --purge gnome-2048 aisleriot atomix gnome-chess \
+    five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg \
+    gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots \
+    gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex \
+    gnome-games -yy
+
+    echo "Gnome is starting to feel lighter..."
+    echo "let's get rid of some more."
+    sleep 1.5
+
+    sudo apt remove --purge cheese libreoffice-gnome libreoffice-impress \
+    libreoffice-writer gnome-clocks gnome-documents gnome-maps gnome-music \
+    gnome-sound-recorder gnome-todo shotwell polari gnome-remote-desktop
+    empathy vino vinagre -yy
+
+    echo "WOW that feels better."
+    echo "...gonna clean up the rest."
+    sleep 1.5
+
+    sudo apt autoremove && sudo apt autoclean
+else
+    echo "Sorry, this application is only for Debian with a standard Gnome \
+    install."
+fi
